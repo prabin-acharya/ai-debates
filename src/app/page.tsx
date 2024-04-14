a1b6a230ccbc5aa9bd20c768279f40ff54007147
Prabin Acharya <acharyaprabin101@gmail.com>
2024-04-14 16:51:57 +0545

    marquee debates in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import Marquee from "react-fast-marquee";
 
 interface Debate {
   debateId: string;
@@ -43,86 +44,91 @@ export default function Home() {
     fetchDebates();
   }, []);
 
-  const debates2: Debate[] = [
-    {
-      debateId: "JfYFxY_8wyOr40aeNkVKQ",
-      userId: "JXwKfWG-8hu5tYAm0qPDZ",
-      title: "Health vs Wealth #01",
-      createdDateTime: "2024-04-14 06:35:45",
-    },
-    {
-      debateId: "6lySpe_-VDQfas7yjLDt7",
-      userId: "JXwKfWG-8hu5tYAm0qPDZ",
-      title: "AI Safety vs AI Acceleration",
-      createdDateTime: "2024-04-14 06:39:52",
-    },
-    {
-      debateId: "f2zAuEcvqtRDVmFEo64g7",
-      userId: "DIdhnBy1iG7yaJABZeIGK",
-      title: "Health vs Wealth",
-      createdDateTime: "2024-04-14 07:30:56",
-    },
-    {
-      debateId: "RLPvSfReaPLUfQ3SuSPIj",
-      userId: "aa5ImpCPcg0s-rppUr3lk",
-      title: "Health vs Wealth",
-      createdDateTime: "2024-04-13 21:36:42",
-    },
-    {
-      debateId: "WZdHk5MzuxgwqiTiylNT_",
-      userId: "a6FTZrIZuDxQmDkkMoDQt",
-      title: "Health vs Wealth2",
-      createdDateTime: "2024-04-13 21:46:52",
-    },
-    {
-      debateId: "DpaKILDKr-FzpQdasM-xg",
-      userId: "aa5ImpCPcg0s-rppUr3lk",
-      title: "Health vs Wealth3",
-      createdDateTime: "2024-04-13 21:48:23",
-    },
-    {
-      debateId: "2DgwC5s0cuSNAHR8r8CPk",
-      userId: "a6FTZrIZuDxQmDkkMoDQt",
-      title: "health vs wealth4",
-      createdDateTime: "2024-04-13 21:49:28",
-    },
-    {
-      debateId: "AzSEJzzQtzg7QcnQC0Jg9",
-      userId: "F4STq3T1L3mWXG9rRKe77",
-      title: "Health vs Wealth5",
-      createdDateTime: "2024-04-13 21:58:56",
-    },
-    {
-      debateId: "yEoacRFnsGgOSpEQHlB5b",
-      userId: "DoXqa0rSh21ICisX4YE01",
-      title: "health vs wealth6",
-      createdDateTime: "2024-04-13 22:06:18",
-    },
-    {
-      debateId: "QfqgoSixoeCuwX5bvlCqP",
-      userId: "nsc9ZXVvS3FVEU-qSPD63",
-      title: "health vs wealth8",
-      createdDateTime: "2024-04-13 22:09:09",
-    },
-    {
-      debateId: "G8CBwAggCnqQzSL_l8bJX",
-      userId: "P5u4zoz9hyDaEuGDz-OYH",
-      title: "health vs wealth10",
-      createdDateTime: "2024-04-13 22:13:19",
-    },
-    {
-      debateId: "cmRR46vKzE3oXhk9uN1Lc",
-      userId: "li1eXIPADt61jmagJXmKN",
-      title: "health vs wealth11",
-      createdDateTime: "2024-04-13 22:19:53",
-    },
-    {
-      debateId: "2f-KTh6J8YCeZWNLywEnk",
-      userId: "zvUDi_Fq8ABRfM55z5Aly",
-      title: "Health vs Wealth #20",
-      createdDateTime: "2024-04-14 04:04:46",
-    },
-  ];
+  // const debates2: Debate[] = [
+  //   {
+  //     debateId: "JfYFxY_8wyOr40aeNkVKQ",
+  //     userId: "JXwKfWG-8hu5tYAm0qPDZ",
+  //     title: "Health vs Wealth #01",
+  //     createdDateTime: "2024-04-14 06:35:45",
+  //   },
+  //   {
+  //     debateId: "6lySpe_-VDQfas7yjLDt7",
+  //     userId: "JXwKfWG-8hu5tYAm0qPDZ",
+  //     title: "AI Safety vs AI Acceleration",
+  //     createdDateTime: "2024-04-14 06:39:52",
+  //   },
+  //   {
+  //     debateId: "f2zAuEcvqtRDVmFEo64g7",
+  //     userId: "DIdhnBy1iG7yaJABZeIGK",
+  //     title: "Health vs Wealth",
+  //     createdDateTime: "2024-04-14 07:30:56",
+  //   },
+  //   {
+  //     debateId: "RLPvSfReaPLUfQ3SuSPIj",
+  //     userId: "aa5ImpCPcg0s-rppUr3lk",
+  //     title: "Health vs Wealth",
+  //     createdDateTime: "2024-04-13 21:36:42",
+  //   },
+  //   {
+  //     debateId: "WZdHk5MzuxgwqiTiylNT_",
+  //     userId: "a6FTZrIZuDxQmDkkMoDQt",
+  //     title: "Health vs Wealth2",
+  //     createdDateTime: "2024-04-13 21:46:52",
+  //   },
+  //   {
+  //     debateId: "DpaKILDKr-FzpQdasM-xg",
+  //     userId: "aa5ImpCPcg0s-rppUr3lk",
+  //     title: "Health vs Wealth3",
+  //     createdDateTime: "2024-04-13 21:48:23",
+  //   },
+  //   {
+  //     debateId: "2DgwC5s0cuSNAHR8r8CPk",
+  //     userId: "a6FTZrIZuDxQmDkkMoDQt",
+  //     title: "health vs wealth4",
+  //     createdDateTime: "2024-04-13 21:49:28",
+  //   },
+  //   {
+  //     debateId: "AzSEJzzQtzg7QcnQC0Jg9",
+  //     userId: "F4STq3T1L3mWXG9rRKe77",
+  //     title: "Health vs Wealth5",
+  //     createdDateTime: "2024-04-13 21:58:56",
+  //   },
+  //   {
+  //     debateId: "yEoacRFnsGgOSpEQHlB5b",
+  //     userId: "DoXqa0rSh21ICisX4YE01",
+  //     title: "health vs wealth6",
+  //     createdDateTime: "2024-04-13 22:06:18",
+  //   },
+  //   {
+  //     debateId: "QfqgoSixoeCuwX5bvlCqP",
+  //     userId: "nsc9ZXVvS3FVEU-qSPD63",
+  //     title: "health vs wealth8",
+  //     createdDateTime: "2024-04-13 22:09:09",
+  //   },
+  //   {
+  //     debateId: "G8CBwAggCnqQzSL_l8bJX",
+  //     userId: "P5u4zoz9hyDaEuGDz-OYH",
+  //     title: "health vs wealth10",
+  //     createdDateTime: "2024-04-13 22:13:19",
+  //   },
+  //   {
+  //     debateId: "cmRR46vKzE3oXhk9uN1Lc",
+  //     userId: "li1eXIPADt61jmagJXmKN",
+  //     title: "health vs wealth11",
+  //     createdDateTime: "2024-04-13 22:19:53",
+  //   },
+  //   {
+  //     debateId: "2f-KTh6J8YCeZWNLywEnk",
+  //     userId: "zvUDi_Fq8ABRfM55z5Aly",
+  //     title: "Health vs Wealth #20",
+  //     createdDateTime: "2024-04-14 04:04:46",
+  //   },
+  // ];
+
+  console.log(debates);
+  const debatesLength = debates.length;
+  const debates1 = debates.slice(0, debatesLength / 2);
+  const debates2 = debates.slice(debatesLength / 2, debatesLength);
 
   return (
     <main className="flex min-h-screen flex-col bg-slate-300">
@@ -141,7 +147,29 @@ export default function Home() {
         </nav>
 
         <div className="border border-red-950 min-h-96 pt-8 pb-40">
-          <div className="flex flex-wrap m-auto justify-center">
+          <Marquee pauseOnHover={true}>
+            {debates1.map((debate) => (
+              <div
+                key={debate.debateId}
+                onClick={() => router.push(`/debate/${debate.debateId}`)}
+                className="bg-gray-200-400 px-2  py-2 m-2 rounded-md inline-block cursor-pointer shadow-md hover:shadow-slate-400"
+              >
+                <div className="w-64 h-20 relative m-auto py-2">
+                  <Image
+                    fill={true}
+                    className="absolute inset-0 rounded object-cover"
+                    src={`https://pub-2f1faf404e074e64b3a0f184d00d15e4.r2.dev/${debate.debateId}`}
+                    alt={"hello"}
+                  />
+                </div>
+
+                <div className="w-full text-center">
+                  <p className="font-semibold mt-2">{debate.title}</p>
+                </div>
+              </div>
+            ))}
+          </Marquee>
+          <Marquee direction="right" pauseOnHover={true}>
             {debates2.map((debate) => (
               <div
                 key={debate.debateId}
@@ -162,7 +190,29 @@ export default function Home() {
                 </div>
               </div>
             ))}
-          </div>
+          </Marquee>
+          {/* <div className="flex flex-wrap m-auto justify-center">
+            {debates2.map((debate) => (
+              <div
+                key={debate.debateId}
+                onClick={() => router.push(`/debate/${debate.debateId}`)}
+                className="bg-gray-200-400 px-2  py-2 m-2 rounded-md inline-block cursor-pointer shadow-md hover:shadow-slate-400"
+              >
+                <div className="w-64 h-20 relative m-auto py-2">
+                  <Image
+                    fill={true}
+                    className="absolute inset-0 rounded object-cover"
+                    src={`https://pub-2f1faf404e074e64b3a0f184d00d15e4.r2.dev/${debate.debateId}`}
+                    alt={"hello"}
+                  />
+                </div>
+
+                <div className="w-full text-center">
+                  <p className="font-semibold mt-2">{debate.title}</p>
+                </div>
+              </div>
+            ))}
+          </div> */}
         </div>
       </div>
     </main>
